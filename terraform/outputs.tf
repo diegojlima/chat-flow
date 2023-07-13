6e8f5d0f287eaa8b8fe6de7553e915de34a7690d
Diego de Lima <diego.delima@acrolinx.com>
2023-07-13 13:57:33 +0200

    fix: terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,5 @@
 
 output "lambda_function_name" {
-  value = aws_lambda_function.chat_flow_service.function_name
+  value = aws_lambda_function.cache_lambda.function_name
+  description = "The name of the Lambda function"
 }
