ad1ab2c55f8373cf5ad29306b3610e0f159b7204
Diego de Lima <diego.delima@acrolinx.com>
2023-07-18 15:36:18 +0200

    feat: adding back entire logic

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -17,9 +17,9 @@ export const handler: Handler = async (event: SQSEvent, context: Context): Promi
 
         // Connect to MongoDB
         console.log('Connecting to MongoDB');
-        // const client = await MongoClient.connect(process.env.MONGODB_URI!);
-        // const db: Db = client.db('chatflow');
-        // const conversations: Collection<Conversation> = db.collection('conversations');
+        const client = await MongoClient.connect(process.env.MONGODB_URI!);
+        const db: Db = client.db('chatflow');
+        const conversations: Collection<Conversation> = db.collection('conversations');
 
         // Extract the conversation ID and message from the SQS message
         console.log('Extracting conversation ID and message from SQS message');
@@ -31,62 +31,62 @@ export const handler: Handler = async (event: SQSEvent, context: Context): Promi
         // Delete conversations that are more than an hour old
         console.log('Deleting old conversations');
         const cutoff_time: Date = new Date(Date.now() - 60 * 60 * 1000);
-        // await conversations.deleteMany({ start_time: { $lt: cutoff_time } });
+        await conversations.deleteMany({ start_time: { $lt: cutoff_time } });
 
         // Check if the conversation is already in the database
         console.log('Checking if conversation is already in the database');
-        // let conversation = await conversations.findOne({ _id: conversation_id });
+        let conversation = await conversations.findOne({ _id: conversation_id });
 
-        // if (conversation === null) {
-        //     // If not, create a new conversation with the current message
-        //     console.log('Conversation not found, creating new conversation');
-        //     conversation = {
-        //         _id: conversation_id,
-        //         messages: [message],
-        //         start_time: new Date(),
-        //     };
-        //     await conversations.insertOne(conversation);
-        // } else {
-        //     // If so, update the conversation with the current message
-        //     console.log('Updating existing conversation');
-        //     conversation.messages.push(message);
-        //     await conversations.updateOne(
-        //         { _id: conversation_id },
-        //         { $set: { messages: conversation.messages } },
-        //     );
-        // }
+        if (conversation === null) {
+            // If not, create a new conversation with the current message
+            console.log('Conversation not found, creating new conversation');
+            conversation = {
+                _id: conversation_id,
+                messages: [message],
+                start_time: new Date(),
+            };
+            await conversations.insertOne(conversation);
+        } else {
+            // If so, update the conversation with the current message
+            console.log('Updating existing conversation');
+            conversation.messages.push(message);
+            await conversations.updateOne(
+                { _id: conversation_id },
+                { $set: { messages: conversation.messages } },
+            );
+        }
 
         // Prepare the messages to send to the processed queue
         console.log('Preparing messages to send to processed queue');
-        // const messagesToSend = conversation.messages.slice(-4);
+        const messagesToSend = conversation.messages.slice(-4);
 
         // Write the last 4 messages to the processed queue
         console.log('Sending messages to processed queue');
-        // const sendMessageReq: SendMessageRequest = {
-        //     QueueUrl: process.env.PROCESSED_QUEUE_URL!,
-        //     MessageBody: JSON.stringify(messagesToSend),
-        // };
+        const sendMessageReq: SendMessageRequest = {
+            QueueUrl: process.env.PROCESSED_QUEUE_URL!,
+            MessageBody: JSON.stringify(messagesToSend),
+        };
 
-        // try {
-        //     await sqs.sendMessage(sendMessageReq).promise();
-        //     console.log('Successfully sent messages to processed queue');
-        // } catch (error: any) {
-        //     console.error(`Failed to send messages to processed queue: ${error}`);
-        //     return;
-        // }
+        try {
+            await sqs.sendMessage(sendMessageReq).promise();
+            console.log('Successfully sent messages to processed queue');
+        } catch (error: any) {
+            console.error(`Failed to send messages to processed queue: ${error}`);
+            return;
+        }
 
         // Delete the message from the interaction queue
         console.log('Deleting message from interaction queue');
-        // try {
-        //     await sqs.deleteMessage({
-        //         QueueUrl: process.env.INTERACTION_QUEUE_URL!,
-        //         ReceiptHandle: event.Records[0].receiptHandle,
-        //     }).promise();
-        //     console.log('Successfully deleted message from interaction queue');
-        // } catch (error: any) {
-        //     console.error(`Failed to delete message from interaction queue: ${error}`);
-        //     return;
-        // }
+        try {
+            await sqs.deleteMessage({
+                QueueUrl: process.env.INTERACTION_QUEUE_URL!,
+                ReceiptHandle: event.Records[0].receiptHandle,
+            }).promise();
+            console.log('Successfully deleted message from interaction queue');
+        } catch (error: any) {
+            console.error(`Failed to delete message from interaction queue: ${error}`);
+            return;
+        }
         
         console.log('Execution complete');
     } catch (error: any) {
