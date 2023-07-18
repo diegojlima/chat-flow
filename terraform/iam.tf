ab26a033f5ee06a2f809d00728ce99be11f37eae
Diego de Lima <diego.delima@acrolinx.com>
2023-07-18 19:55:36 +0200

    feat: add restructured terraform with vpc

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,3 +13,59 @@ resource "aws_iam_role" "lambda_exec_role" {
   name               = "lambda_exec_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_role_sqs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_role_lambda_basic" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_role_secrets_manager" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_role_vpc_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_role_sqs_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
+}
+
+resource "aws_iam_policy" "docdb_access" {
+  name        = "docdb_access"
+  description = "Policy for accessing DocumentDB"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "DocDBAccess",
+            "Effect": "Allow",
+            "Action": [
+                "rds-data:*",
+                "rds:CreateDBCluster",
+                "rds:DescribeDBClusters",
+                "rds:DeleteDBCluster",
+                "rds:StartDBCluster",
+                "rds:StopDBCluster"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_role_docdb_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.docdb_access.arn
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,3 @@
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -27,27 +26,13 @@ resource "aws_nat_gateway" "nat" {
   subnet_id     = aws_subnet.main.id
 }
 
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "main"
-  }
+data "aws_route_table" "existing" {
+  route_table_id = "rtb-09a23bf061b3f7bf5"
 }
 
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
+  route_table_id = data.aws_route_table.existing.id
 }
 
 resource "aws_security_group" "sg" {
