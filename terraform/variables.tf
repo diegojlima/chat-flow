1072fe253ed848ade970c665bc9a5f1ddbbfe044
Diego de Lima <diego.delima@acrolinx.com>
2023-07-18 19:22:53 +0200

    feat: add restructured terraform with vpc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -40,10 +40,3 @@ variable "mongodb_password" {
   description = "The password for MongoDB"
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-}
-
-variable "subnet_cidr_block" {
-  description = "CIDR block for the subnet within the VPC"
-}
